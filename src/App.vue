12ebcf0161d2e9e09e2c34bcea57c805ee8ab54a
gitcseme <shuvo_CSE23@yahoo.com>
2022-05-27 18:32:33 +0600

    configured client project witrh basic functionality

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div class="container-fluid con-bg">
+      <div class="row">
+        <div class="col-12">
+          <h2>App vue</h2>
+          <router-view/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,16 +20,8 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+.con-bg {
+  background-color: #2c3e50;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// import configurations
+import './configurations/Axios'
+
+// UI library
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
 
 new Vue({
